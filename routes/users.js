93d820369dd88537a09e62bd75b54b47156e4fac
hercules <kjmhercules@gmail.com>
2017-08-09 01:23:47 +0900

    users 페이지 추가

--- routes/users.js
+++ routes/users.js
@@ -8,28 +8,28 @@ router.get('/', function(req, res, next) {
 	res.render('users/index', {title: 'Users', users: userList});
 });
 
-router.get('/new', function(req, res) {
-	res.render('users/new', {title: 'New Users'});
-});
+//router.get('/new', function(req, res) {
+//	res.render('users/new', {title: 'New Users'});
+//});
 
-router.post('/', function(req, res){
-	if (userList[req.body.username]) {
-		res.send('Conflict', 409);
-	} else {
-		userList[req.body.username] = req.body;
-		res.redirect('/users');
-	}
-});
-
-
-router.get('/:name', function(req, res, next) {
-	var user = userList[req.params.name];
-	if (user) {
-		res.render('users/profile', {title: 'User Profile', user: user});
-	} else {
-		next();
-	}
-});
+//router.post('/', function(req, res){
+//	if (userList[req.body.username]) {
+//		res.send('Conflict', 409);
+//	} else {
+//		userList[req.body.username] = req.body;
+//		res.redirect('/users');
+//	}
+//});
+//
+//
+//router.get('/:name', function(req, res, next) {
+//	var user = userList[req.params.name];
+//	if (user) {
+//		res.render('users/profile', {title: 'User Profile', user: user});
+//	} else {
+//		next();
+//	}
+//});
 
 
 module.exports = router;
